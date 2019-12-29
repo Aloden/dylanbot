577a94ca7ef57c2209a2df4025051d70bb595646
Aloden <46196350+Aloden@users.noreply.github.com>
2019-12-29 17:01:54 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const Enmap = require('enmap');
+const fs = require('fs');
+
+const client = new Discord.Client();
+const config = require('./config.json');
+
+client.config = config;
+
+fs.readdir('./events/', (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith('.js')) return;
+    const event = require(`./events/${file}`);
+    let eventName = file.split('.')[0];
+    console.log(`En train de charger l'événement ` + file)
+    client.on(eventName, event.bind(null, client));
+  });
+});
+
+client.commands = new Enmap();
+
+fs.readdir('./commands/', (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith('.js')) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split('.')[0];
+    console.log(`En train de charger la commande ${commandName}`);
+    client.commands.set(commandName, props);
+  });
+});
+
+client.login(config.token);
